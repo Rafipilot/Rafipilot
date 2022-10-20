ebc9527aea6dbe604a731afc7fdc57bbba59e3c8
Rafipilot <74052861+Rafipilot@users.noreply.github.com>
2022-10-20 12:21:12 +0100

    email

--- contact-form-process.php
+++ contact-form-process.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE FOLLOWING TWO LINES:
-    $email_to = "";
+    $email_to = "Rafayel.latif@gmail.com";
     $email_subject = "New form submissions";
 
     function problem($error)
